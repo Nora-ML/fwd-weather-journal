4e62fb7ee0f9727ffb15badd736b0f5220ba0e14
Nora-ML <86887904+Nora-ML@users.noreply.github.com>
2021-11-20 14:41:02 +0200

    Altered port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var projectData = {};
 /**setting express as Local server */
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, lisening => {
     console.log('running local host port: ' + port);
 })
